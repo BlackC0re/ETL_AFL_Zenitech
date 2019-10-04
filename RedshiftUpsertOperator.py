36f88e39e4119b466c12b0128b35c49d6ea12917
BlackC0re <bacu.andrei@gmail.com>
2019-10-04 12:25:33 +0300

    Update RedshiftUpsertOperator.py

--- RedshiftUpsertOperator.py
+++ RedshiftUpsertOperator.py
@@ -1,3 +1,7 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
 class RedshiftUpsertOperator(BaseOperator):
  
 """
