1be8adbe829674d681c5bff68b181c0858d7eda3
BlackC0re <bacu.andrei@gmail.com>
2019-10-08 18:46:28 +0300

    Update RedshiftUpsertOperator.py

--- RedshiftUpsertOperator.py
+++ RedshiftUpsertOperator.py
@@ -8,60 +8,44 @@ class RedshiftUpsertOperator(BaseOperator):
  
 Executes an upsert from one to another table in the same Redshift database by completely replacing the rows of target table with the rows in staging table that contain the same business key
  
-:param src_redshift_conn_id: reference to a specific Redshift database
-:type src_redshift_conn_id: string
+:param redshift_conn_id: reference to a specific Redshift database
+:type redshift_conn_id: string
  
-:param dest_redshift_conn_id: reference to a specific Redshift database
-:type dest_redshift_conn_id: string
- 
-:param src_table: reference to a specific Redshift table
-:type src_table: string
+:param aws_iam_role: reference to a specific IAM role
+:type aws_iam_role: string
  
 :param dest_table: reference to a specific Redshift table
 :type dest_table: string
  
-:param src_keys business keys that are supposed to be matched with dest_keys business keys in the target table
-:type src_keys: string
- 
-:param dest_keys business keys that are supposed to be matched with src_keys business keys in the source table
-:type dest_keys: string
+:param sql_query: SQL query to be executed
+:type sql_query: string
+
 """
  
 @apply_defaults
- 
-def __init__(self, src_redshift_conn_id, dest_redshift_conn_id,
-  src_table, dest_table, src_keys, dest_keys, *args, **kwargs):
- 
-  self.src_redshift_conn_id = src_redshift_conn_id
-  self.dest_redshift_conn_id = dest_redshift_conn_id
-  self.src_table = src_table
-  self.dest_table = dest_table 
-  self.src_keys = src_keys
-  self.dest_keys = dest_keys
-  super(RedshiftUpsertOperator , self).__init__(*args, **kwargs)
- 
+def __init__(self,
+             redshift_conn_id="my_rs_conn",
+             aws_iam_role="arn:aws:iam::1234:role/myRSRole",
+             dest_table='Dim_User',
+             sql_query='query_text',
+             *args, **kwargs):
+
+    super(LoadDimensionOperator, self).__init__(*args, **kwargs)
+    self.redshift_conn_id=redshift_conn_id
+    self.aws_iam_role=aws_iam_role
+    self.dest_table=dest_table
+    self.insert_mode=insert_mode
+    self.sql_query=sql_query
+        
 def execute(self, context):
-  self.hook = PostgresHook(postgres_conn_id=self.src_redshift_conn_id)
+  self.hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
   conn = self.hook.get_conn()
   cursor = conn.cursor()
-  log.info("Connected with " + self.src_redshift_conn_id)
-  # build the SQL statement
-  sql_statement = "BEGIN TRANSACTION; "
-  sql_statement += "DELETE FROM " + self.dest_table + " using " + self.src_table + " where "
-  for i in range (0,len(self.src_keys)):
-    sql_statement += self.src_table + "." + self.src_keys[i] + " = " + self.dest_table + "." + self.dest_keys[i]
-    if(i < len(self.src_keys)-1):
-      sql_statement += " and "
-      
-  sql_statement += "; "
-  sql_statement += " INSERT INTO " + self.dest_table + " select * from " + self.src_table + " ; "
-  sql_statement += " END TRANSACTION; "
- 
-  print(sql_statement)
-  cursor.execute(sql_statement)
+  log.info("Connected with " + self.redshift_conn_id)
+  cursor.execute(sql_query)
   cursor.close()
   conn.commit()
-  log.info("Upsert command completed")
+  log.info("Upsert completed successfully!")
  
 class RedshiftUpsertPlugin(AirflowPlugin):
   name = "redshift_upsert_plugin"
